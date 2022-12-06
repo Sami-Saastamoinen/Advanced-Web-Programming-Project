df5a69d79e070251f05b3317725f1c1877bff8c3
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2022-12-06 19:07:33 +0200

    index html clean-up, nav buttons & styling

--- final-project/src/pages/Home.js
+++ final-project/src/pages/Home.js
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
+	const navigate = useNavigate();
+
 	return (
 		<div>
 			<h1>Home Page</h1>
+			<button className="home__button" onClick={() => navigate("/store")}>
+				Check Out Our Selection
+			</button>
+			<button className="home__button" onClick={() => navigate("/cart")}>
+				Go To Your Cart
+			</button>
+			<button className="home__button" onClick={() => navigate("/admin")}>
+				Admin Page | Plz Don't Use :)
+			</button>
 		</div>
 	);
 };
