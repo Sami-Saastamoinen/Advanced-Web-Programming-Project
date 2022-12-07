82492c2831ca462311cf4573375e8237bb55bcac
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2022-12-07 19:45:21 +0200

    styling

--- final-project/src/components/Products.js
+++ final-project/src/components/Products.js
@@ -7,18 +7,27 @@ const Products = (props) => {
 		<ul>
 			{props.products.map((product) => (
 				<li key={product.id}>
-					<h2>{product.name}</h2>
-					<p>{product.plastic}</p>
-					<p>{product.description}</p>
-					<p>{product.condition}</p>
-					<p>${product.price}</p>
-					{props.showAddToCart && <AddToCart product={product} />}
-					{props.showRemoveFromCart && (
-						<RemoveFromCart product={product} />
-					)}
-					{props.showRemoveProduct && (
-						<RemoveProduct product={product} />
-					)}
+					<div className="row">
+						<div className="column">
+							<h2>{product.name}</h2>
+							<h2>{product.plastic}</h2>
+							<h3>{product.description}</h3>
+							<h3>
+								{product.condition} | ${product.price}
+							</h3>
+						</div>
+						<div className="column">
+							{props.showAddToCart && (
+								<AddToCart product={product} />
+							)}
+							{props.showRemoveFromCart && (
+								<RemoveFromCart product={product} />
+							)}
+							{props.showRemoveProduct && (
+								<RemoveProduct product={product} />
+							)}
+						</div>
+					</div>
 				</li>
 			))}
 		</ul>
